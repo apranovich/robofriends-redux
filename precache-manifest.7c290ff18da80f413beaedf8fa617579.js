7333732b51640a3575221ec62945c2c5dff47305
Artyom Pranovich <Artsiom.Pranovich@apps.ask.com>
2019-06-02 17:38:17 +0300

    Updates

---
+++ precache-manifest.7c290ff18da80f413beaedf8fa617579.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "57e39479f834550e459915de8498df34",
+    "url": "/apranovich/robofriends-redux/index.html"
+  },
+  {
+    "revision": "d957ad2f2306b2416b43",
+    "url": "/apranovich/robofriends-redux/static/css/2.2ec98f22.chunk.css"
+  },
+  {
+    "revision": "831e1c341540cacef602",
+    "url": "/apranovich/robofriends-redux/static/css/main.eec1f484.chunk.css"
+  },
+  {
+    "revision": "d957ad2f2306b2416b43",
+    "url": "/apranovich/robofriends-redux/static/js/2.9f060845.chunk.js"
+  },
+  {
+    "revision": "831e1c341540cacef602",
+    "url": "/apranovich/robofriends-redux/static/js/main.23ef7a3b.chunk.js"
+  },
+  {
+    "revision": "5f349f9659ae1e731724",
+    "url": "/apranovich/robofriends-redux/static/js/runtime~main.22939dd0.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/apranovich/robofriends-redux/static/media/SEGA.12a3db8b.woff"
+  }
+]);
